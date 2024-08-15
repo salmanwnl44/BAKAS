86b85c8679211b777bf3a75d36ef7d43117e0a74
Salman Ansari <136922694+salmanwnl44@users.noreply.github.com>
2024-08-15 18:25:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URL = 'mongodb+srv://salmanansariemailpc:DeIOgSGRBoL4BVSa@cluster0.bruz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+DATABASE_NAME = 'Cluster0'
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
